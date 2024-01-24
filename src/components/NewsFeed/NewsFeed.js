087b1c05516bb509cd0d2b59fe714ea8451fe951
Dmiriy <152209418+DmitriyKash@users.noreply.github.com>
2024-01-24 19:30:00 +0200

    Update NewsFeed.js
    Додав Условний APi

--- src/components/NewsFeed/NewsFeed.js
+++ src/components/NewsFeed/NewsFeed.js
@@ -5,11 +5,15 @@ const NewsFeed = () => {
     const [news, setNews] = useState([]);
 
     useEffect(() => {
-        fetch('https://example.com/api/news') // Заменить на реальный API
+        const apiKey = '85f5aebd9d3d4d74a93715856a5a5693';
+        const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`;
+
+        fetch(url)
             .then(response => response.json())
-            .then(data => setNews(data))
+            .then(data => setNews(data.articles)) // Предполагается, что ответ содержит поле 'articles'
             .catch(error => console.error('Ошибка:', error));
     }, []);
+
 
     return (
         <div className="news-feed">
