492a672d8069606ba14db561095ad7102ab9a628
Dmiriy <152209418+DmitriyKash@users.noreply.github.com>
2024-01-30 15:49:14 +0200

    Update NewsFeed.js

--- src/components/NewsFeed/NewsFeed.js
+++ src/components/NewsFeed/NewsFeed.js
@@ -9,7 +9,7 @@ const NewsFeed = () => {
 
     useEffect(() => {
         const apiKey = '85f5aebd9d3d4d74a93715856a5a5693';
-        const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`;
+        const url = `https://newsapi.org/v2/top-headlines?country=ua&apiKey=${apiKey}`;
 
 
 
