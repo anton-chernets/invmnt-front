2f89054cf737baf9a4b5e5bc7a70abc79abfc24a
Dmiriy <152209418+DmitriyKash@users.noreply.github.com>
2024-02-10 13:38:14 +0200

    Update News

--- src/components/NewsItem/NewsItem.js
+++ src/components/NewsItem/NewsItem.js
@@ -1,36 +1,29 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './NewsItem.css';
 import defaultImage from '../../img/image_2024-02-07_10-47-09.png';
-import { Link } from 'react-router-dom';
+
+// Встановлення максимальної висоти для прев'ю
+const MAX_PREVIEW_HEIGHT = '100px';
 
 export const NewsItem = ({ id, title, description, imageUrl }) => {
     const [isExpanded, setIsExpanded] = useState(false);
-    // You need to add this state to manage the maxHeight
-    const [maxHeight, setMaxHeight] = useState("0px");
     const contentRef = useRef(null);
 
-    const toggleExpand = () => {
-        setIsExpanded(prev => !prev); // Toggle the expanded state
-        // Update the maxHeight depending on the expanded state
-        setMaxHeight(
-            isExpanded ? "0px" : `${contentRef.current.scrollHeight}px`
-        );
-    };
+    useEffect(() => {
+        // Початково встановлюємо максимальну висоту, щоб показати частину тексту
+        contentRef.current.style.maxHeight = isExpanded ? `${contentRef.current.scrollHeight}px` : MAX_PREVIEW_HEIGHT;
+    }, [isExpanded]);
 
-    const renderedDescription = isExpanded ? description : description.substring(0, 100) + '...';
+    const toggleExpand = () => {
+        setIsExpanded(!isExpanded);
+    };
 
     return (
         <div className="news-item">
-            <Link to={`/news/${id}`} style={{ textDecoration: 'none' }}>
-                <h2>{title}</h2>
-            </Link>
+            <h2>{title}</h2>
             <img src={imageUrl || defaultImage} alt={title || 'Default'} className="news-image" />
-            <div
-                ref={contentRef}
-                style={{ maxHeight: `${maxHeight}` }} // Use the maxHeight state here
-                className="content"
-            >
-                <p>{renderedDescription}</p>
+            <div ref={contentRef} className="content">
+                <p>{description}</p>
             </div>
             {description.length > 100 && (
                 <button onClick={toggleExpand} className="toggle-content-btn">
