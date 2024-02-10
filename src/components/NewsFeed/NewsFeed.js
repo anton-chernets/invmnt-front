d371436f0cd5b3eb2157f9d6a84dfc32068e486e
Dmiriy <152209418+DmitriyKash@users.noreply.github.com>
2024-02-10 11:43:18 +0200

    Update News

--- src/components/NewsFeed/NewsFeed.js
+++ src/components/NewsFeed/NewsFeed.js
@@ -1,55 +1,43 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+
 import './NewsFeed.css';
 import Sidebar from "../Sidebar/Sidebar";
 import defaultImage from '../../img/image_2024-02-07_10-47-09.png';
-
+import NewsItem from "../NewsItem/NewsItem";
 
 
 const NewsFeed = () => {
-
     const [news, setNews] = useState([]);
 
     useEffect(() => {
-        const url = 'http://95.217.181.158/api/articles';
+        const fetchNews = async () => {
+            const url = 'http://95.217.181.158/api/articles';
+            try {
+                const response = await fetch(url);
+                const data = await response.json();
+                setNews(data?.data || []);
+            } catch (error) {
+                console.error('Error fetching news:', error);
+            }
+        };
 
-
-        fetch(url)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error(`HTTP error! Status: ${response.status}`);
-                }
-                return response.json();
-
-            })
-            .then(data => {
-                if (data?.data && Array.isArray(data.data)) {
-                    setNews(data.data);
-                    console.log(data)
-                } else {
-                    console.error('Field "data" is missing from the response');
-                }
-            })
-            .catch(error => console.error('Error:', error));
+        fetchNews();
     }, []);
+
     return (
         <div className="news-container">
-
             <div className="news-items">
-                {news.map((item) => {
-                    const imageUrl = item.images && item.images.length > 0 ? item.images[0] : defaultImage;
-                    return (
-                        <div className="news-item" key={item.id}>
-                            <Link to={`/news/${item.id}`} style={{textDecoration: 'none'}}>
-                                <h2>{item.title}</h2>
-                                <img src={imageUrl} alt={item.title || 'Default'} className="news-image"/>
-                                <p>{item.description}</p>
-                            </Link>
-                        </div>
-                    );
-                })}
+                {news.map((item) => (
+                    <NewsItem
+                        key={item.id}
+                        id={item.id}
+                        title={item.title}
+                        description={item.description}
+                        imageUrl={item.images && item.images.length > 0 ? item.images[0] : defaultImage}
+                    />
+                ))}
             </div>
-            <Sidebar/>
+            <Sidebar />
         </div>
     );
 };
