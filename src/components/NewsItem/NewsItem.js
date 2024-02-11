16052f817345b7ae5244bcad6e9ffe27d38fc2f4
Dmiriy <152209418+DmitriyKash@users.noreply.github.com>
2024-02-11 11:50:18 +0200

    Update

--- src/components/NewsItem/NewsItem.js
+++ src/components/NewsItem/NewsItem.js
@@ -1,17 +1,19 @@
 import React, { useState, useRef, useEffect } from 'react';
 import './NewsItem.css';
 import defaultImage from '../../img/image_2024-02-07_10-47-09.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDown, faAngleUp } from '@fortawesome/free-solid-svg-icons';
 
-// Встановлення максимальної висоти для прев'ю
-const MAX_PREVIEW_HEIGHT = '85px';
-
-export const NewsItem = ({ id, title, description, imageUrl }) => {
+const NewsItem = ({ id, title, description, imageUrl }) => {
     const [isExpanded, setIsExpanded] = useState(false);
     const contentRef = useRef(null);
 
     useEffect(() => {
-        // Початково встановлюємо максимальну висоту, щоб показати частину тексту
-        contentRef.current.style.maxHeight = isExpanded ? `${contentRef.current.scrollHeight}px` : MAX_PREVIEW_HEIGHT;
+        if (isExpanded) {
+            contentRef.current.style.maxHeight = `${contentRef.current.scrollHeight}px`;
+        } else {
+            contentRef.current.style.maxHeight = '55px'; // Set to '0px' to hide content
+        }
     }, [isExpanded]);
 
     const toggleExpand = () => {
@@ -19,17 +21,23 @@ export const NewsItem = ({ id, title, description, imageUrl }) => {
     };
 
     return (
-        <div className="news-item">
+        <div className={`news-item ${isExpanded ? 'expanded' : ''}`}>
             <h2>{title}</h2>
             <img src={imageUrl || defaultImage} alt={title || 'Default'} className="news-image" />
             <div ref={contentRef} className="content">
                 <p>{description}</p>
             </div>
-            {description.length > 100 && (
-                <button onClick={toggleExpand} className="toggle-content-btn">
-                    {isExpanded ? 'Згорнути' : 'Розгорнути'}
-                </button>
-            )}
+            <button onClick={toggleExpand} className="toggle-content-btn">
+                {isExpanded ? (
+                    <>
+                        Згорнути <FontAwesomeIcon icon={faAngleUp} />
+                    </>
+                ) : (
+                    <>
+                        Розгорнути <FontAwesomeIcon icon={faAngleDown} />
+                    </>
+                )}
+            </button>
         </div>
     );
 };
