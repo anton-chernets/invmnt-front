9138f53c7884192d6735a5129bf54e620bb53658
Dmiriy <152209418+DmitriyKash@users.noreply.github.com>
2024-02-20 15:05:32 +0200

    Update AD

--- src/components/AdComponent/AdComponent.jsx
+++ src/components/AdComponent/AdComponent.jsx
@@ -1,25 +1,54 @@
+// import React, { useEffect } from 'react';
+
+// const AdComponent = () => {
+//     useEffect(() => {
+//         if (window.adsbygoogle && !window.adsbygoogleLoaded) {
+//             window.adsbygoogleLoaded = true; // Mark as loaded to avoid future loads
+//             try {
+//                 (window.adsbygoogle = window.adsbygoogle || []).push({});
+//             } catch (error) {
+//                 console.error('Error loading adsbygoogle script:', error);
+//             }
+//         }
+//     }, []);
+
+//     return (
+//         <ins className="adsbygoogle"
+//             style={{ display: "block" }}
+//             data-ad-client="ca-pub-9704083006647691"
+//             data-ad-slot="6491375208"
+//             data-ad-format="auto"
+//             data-full-width-responsive="true"></ins>
+//     );
+// };
+
+// export default AdComponent;
+
+
 import React, { useEffect } from 'react';
 
 const AdComponent = () => {
-    useEffect(() => {
-        if (window.adsbygoogle && !window.adsbygoogleLoaded) {
-            window.adsbygoogleLoaded = true; // Mark as loaded to avoid future loads
-            try {
-                (window.adsbygoogle = window.adsbygoogle || []).push({});
-            } catch (error) {
-                console.error('Error loading adsbygoogle script:', error);
-            }
-        }
-    }, []);
+  useEffect(() => {
+    try {
+      // Ensure the adsbygoogle script is loaded and only initialize once
+      if (window.adsbygoogle && !window.adsbygoogleLoaded) {
+        window.adsbygoogleLoaded = true; // Prevent future loads
+        (window.adsbygoogle = window.adsbygoogle || []).push({});
+      }
+    } catch (error) {
+      console.error('Error loading adsbygoogle script:', error);
+    }
+  }, []);
 
-    return (
-        <ins className="adsbygoogle"
-            style={{ display: "block" }}
-            data-ad-client="ca-pub-9704083006647691"
-            data-ad-slot="6491375208"
-            data-ad-format="auto"
-            data-full-width-responsive="true"></ins>
-    );
+  return (
+    <ins className="adsbygoogle"
+         style={{ display: 'block' }}
+         data-ad-client="ca-pub-9704083006647691"
+         data-ad-slot="6491375208"
+         data-ad-format="auto"
+         data-full-width-responsive="true"></ins>
+  );
 };
 
 export default AdComponent;
+
