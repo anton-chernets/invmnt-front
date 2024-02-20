073279c7ab97544acaefb48a7446988bfba6a282
Dmiriy <152209418+DmitriyKash@users.noreply.github.com>
2024-02-20 13:57:13 +0200

    Add NewsAPI

--- src/components/NewsItem/NewsItem.jsx
+++ src/components/NewsItem/NewsItem.jsx
@@ -1,48 +1,59 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, {  } from 'react';
+// useState, useRef, useEffect
 import './NewsItem.css';
 import defaultImage from '../../img/image_2024-02-07_10-47-09.png';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngleDown, faAngleUp } from '@fortawesome/free-solid-svg-icons';
-import {Link} from "react-router-dom";
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faAngleDown, faAngleUp } from '@fortawesome/free-solid-svg-icons';
+// import {Link} from "react-router-dom";
 
-const NewsItem = ({ id, title, description, imageUrl }) => {
-    const [isExpanded, setIsExpanded] = useState(false);
-    const contentRef = useRef(null);
+// const NewsItem = ({ id, title, description, imageUrl }) => {
+//     const [isExpanded, setIsExpanded] = useState(false);
+//     const contentRef = useRef(null);
 
-    useEffect(() => {
-        if (isExpanded) {
-            contentRef.current.style.maxHeight = `${contentRef.current.scrollHeight}px`;
-        } else {
-            contentRef.current.style.maxHeight = '55px';
-        }
-    }, [isExpanded]);
+//     useEffect(() => {
+//         if (isExpanded) {
+//             contentRef.current.style.maxHeight = `${contentRef.current.scrollHeight}px`;
+//         } else {
+//             contentRef.current.style.maxHeight = '55px';
+//         }
+//     }, [isExpanded]);
 
-    const toggleExpand = () => {
-        setIsExpanded(!isExpanded);
-    };
+//     const toggleExpand = () => {
+//         setIsExpanded(!isExpanded);
+//     };
 
+//     return (
+//         <div className={`news-item ${isExpanded ? 'expanded' : ''}`}>
+//             <Link to={`/news/${id}`} style={{ textDecoration: 'none' }}>
+//                 <h3>{title}</h3>
+//                 <img src={imageUrl || defaultImage} alt={title || 'Default'} className="news-image" />
+//             </Link>
+//             <div ref={contentRef} className="content">
+//                 <p>{description}</p>
+//             </div>
+//             <button onClick={toggleExpand} className="toggle-content-btn">
+//                 {isExpanded ? (
+//                     <>
+//                         Згорнути <FontAwesomeIcon icon={faAngleUp} />
+//                     </>
+//                 ) : (
+//                     <>
+//                         Розгорнути <FontAwesomeIcon icon={faAngleDown} />
+//                     </>
+//                 )}
+//             </button>
+//         </div>
+//     );
+// };
+
+const NewsItem = ({ title, description, imageUrl }) => {
     return (
-        <div className={`news-item ${isExpanded ? 'expanded' : ''}`}>
-            <Link to={`/news/${id}`} style={{ textDecoration: 'none' }}>
-                <h3>{title}</h3>
-                <img src={imageUrl || defaultImage} alt={title || 'Default'} className="news-image" />
-            </Link>
-            <div ref={contentRef} className="content">
-                <p>{description}</p>
-            </div>
-            <button onClick={toggleExpand} className="toggle-content-btn">
-                {isExpanded ? (
-                    <>
-                        Згорнути <FontAwesomeIcon icon={faAngleUp} />
-                    </>
-                ) : (
-                    <>
-                        Розгорнути <FontAwesomeIcon icon={faAngleDown} />
-                    </>
-                )}
-            </button>
-        </div>
+      <div className="news-item">
+        <img src={imageUrl || defaultImage} alt={title} className="news-image" />
+        <h3 className="news-title">{title}</h3>
+        <p className="news-description">{description}</p>
+      </div>
     );
-};
+  };
 
 export default NewsItem;
