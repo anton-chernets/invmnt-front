26ea07301d2d8e2593092fec21eb5bfafdb6225c
Dmiriy <152209418+DmitriyKash@users.noreply.github.com>
2024-02-29 19:13:40 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -93,10 +93,12 @@ function App() {
                         <Route path="/products" element={<ProductList />} />
                         <Route path="/search/articles/show/:alias" element={<ArticleDetails />} />
                         {/* <Route path="/search/articles/show/:articleId" element={<ArticleDetails />} /> */}
-                        <Route path="/search/products/show/:productId" element={<ProductDetails />} />
+                        {/* <Route path="/search/products/show/:productId" element={<ProductDetails />} /> */}
+                        <Route path="/search/products/show/:alias" element={<ProductDetails />} />
                         <Route path="/articles/show/:alias" element={<ArticleDetails />} />
                         {/* <Route path="/articles/show/:articleId" element={<ArticleDetails />} /> */}
-                        <Route path="/products/show/:productId" element={<ProductDetails />} />
+                        {/* <Route path="/products/show/:productId" element={<ProductDetails />} /> */}
+                        <Route path="/products/show/:alias" element={<ProductDetails />} />
                         <Route path="/checkout" element={<Checkout/>}/>
                         <Route path="/cart" element={<CartPage/>}/>
                         <Route path="/passwordreset" element={<PasswordReset/>}/>
--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -72,88 +72,6 @@ const ProductList = () => {
         });
     };
 
-    
-
-    // const onDeleteProduct = async (productId) => {
-    //     const authToken = localStorage.getItem('authToken');
-        
-    //     if (!authToken) {
-    //         console.error('Unauthorized: No auth token');
-    //         return;
-    //     }
-    
-    //     if (window.confirm('Ви впевнені, що хочете видалити товар?')) {
-    //         try {
-    //             const response = await fetch(`https://apinvmnt.site/api/products/remove`, {
-    //                 method: 'DELETE',
-    //                 headers: {
-    //                     'Content-Type': 'application/json',
-    //                     'Authorization': `Bearer ${authToken}`
-    //                 },
-    //                 body: JSON.stringify({id: productId})
-    //             });
-    
-    //             if (response.ok) {
-    //                 const data = await response.json();
-    //                 setProducts(currentProducts => currentProducts.filter(product => product.id !== productId));
-    //                 console.log('Product removed successfully:', data);
-    //             } else {
-    //                 // If response is not ok, you could still try to read the response body for error details
-    //                 const errorData = await response.json();
-    //                 throw new Error(errorData.message || 'Failed to delete product');
-    //             }
-    //         } catch (error) {
-    //             console.error('Error deleting product:', error);
-    //         }
-    //     }
-    // };
-    
-    // const handleSaveChanges = async (e) => {
-    //     e.preventDefault();
-    //     await handleUpdateProduct(editingProduct);
-    // };
-    // const handleUpdateProduct = async (updatedProductData) => {
-    //     const payload = {
-    //         title: updatedProductData.title,
-    //         description: updatedProductData.description,
-    //         stock: updatedProductData.stock,
-    //         price: updatedProductData.price,
-    //     };
-
-    //     try {
-    //         const response = await fetch(`https://apinvmnt.site/api/products/update/${updatedProductData.id}`, {
-    //             method: 'POST',
-    //             headers: {
-    //                 'Authorization': `Bearer ${token}`,
-    //                 'Content-Type': 'application/json',
-    //             },
-    //             body: JSON.stringify(payload),
-    //         });
-
-    //         if (!response.ok) {
-    //             throw new Error('Server responded with status: ' + response.status);
-    //         }
-
-    //         const updatedProduct = await response.json();
-    //         setProducts(prevProducts => prevProducts.map(p => p.id === updatedProduct.id ? updatedProduct : p));
-    //     } catch (error) {
-    //         console.error('Error updating product:', error);
-    //         alert('Error updating product: ' + error.message);
-    //     }
-    //     setEditingProduct(null);
-    // };
-    // const handleChange = (e) => {
-    //     const { name, value } = e.target;
-    //     setEditingProduct((prev) => ({
-    //         ...prev,
-    //         [name]: value,
-    //     }));
-    // };
-    // const startEditingProduct = (product) => {
-    //     setEditingProduct(product);
-    // };
-
-    
 
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error: {error.message}</p>;
@@ -170,14 +88,13 @@ const ProductList = () => {
         if (!user) {
             navigate('/login');
         } else {
-            // Retrieve the quantity for the product
-            const quantity = quantities[product.id] || 1; // Default to 1 if not specified
-            onBuyNow(product, quantity); // Pass the quantity to onBuyNow
+            
+            const quantity = quantities[product.id] || 1; 
+            onBuyNow(product, quantity);
         }
     };
 
     async function onBuyNow(product, quantity) {
-        // Ensure a valid quantity is always passed
         const quantityToBuy = quantity || 1;
     
         const orderDetails = {
@@ -226,11 +143,12 @@ const ProductList = () => {
                     <div key={product.id} className="product-item-list">
                         <div className='img-dis'>
                         <Link to={`/products/show/${product.id}`} style={{  textDecoration: 'none' }}>
-                                <img src={product.images[0] || defaultImage} alt={product.title}
+                            <h4>{product.title}</h4>
+                            <img src={product.images[0] || defaultImage} alt={product.title}
                                     className={`product-img`} />
                             
                             
-                            <h3>{product.title}</h3>
+                            
                         </Link>
                         </div><p>{product.description}</p>
                         <p>Ціна: ${product.price}</p>
